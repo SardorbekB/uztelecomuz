7ca7b183cc19762a749978d0333d44579c2ccb33
SardorbekB <qilichovtuit@gmail.com>
2022-09-10 11:54:26 +0500

    six

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.render('forInduviduals.ejs', {
+      title: "Uztelecom - National operator",
+      title1: "Узтелеком - национальный оператор",
+      title2: "Uztelekom - milliy operator"
+  });
+});
+
 const start = async () => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
@@ -9,11 +17,11 @@ const start = async () => {
     app.set('views', __dirname + '/views/pages');
   
     app.use(express.static(__dirname + '/public'));
-  
-    app.use('/', require('./server/routes/forInduviduals'));
-    app.use('/forBusiness', require('./server/routes/forBusiness'));
-    app.use('/forOperators', require('./server/routes/forOperators'));
-    app.use('/aboutCompany', require('./server/routes/aboutCompany'));
+
+    app.use('/en', require('./server/routes/forInduviduals'));
+    app.use('/en/forBusiness', require('./server/routes/forBusiness'));
+    app.use('/en/forOperators', require('./server/routes/forOperators'));
+    app.use('/en/aboutCompany', require('./server/routes/aboutCompany'));
   
     app.listen(process.env.PORT || 3000, () => {
       console.log(`Server ${process.env.PORT | 3000} - portda ishlayapdi`);
